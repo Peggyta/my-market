8082eddb531d93d0f75623ca5a530e188b24ef7f
Peggyta <peggyta9096@gmail.com>
2022-09-25 15:00:35 +0330

    accessing to the information of all products

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,12 +1,23 @@
 import React, {useParams} from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductDetails = () => {
     const params = useParams();
     const id = params.id;
     const data = data[id-1];
+    const {image, title, description, price, category} = product;
     return (
         <div>
-            
+            <img src={image} alt="product"/>
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <p><span>Category:</span>{category}</p>
+                <div>
+                    <span>{price}</span>
+                    <Link to="/products">Back to shop</Link>
+                </div>
+            </div>
         </div>
     );
 };
