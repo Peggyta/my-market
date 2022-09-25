62b448200b004539948e5d916ba9d9cfa249feb0
Peggyta <peggyta9096@gmail.com>
2022-09-25 13:47:23 +0330

    creating a card as a product

---
+++ src/components/shared/Product.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            <img/>
+            <h3>title</h3>
+            <p>price</p>
+            <div>
+                <a>Details</a>
+                <div>
+                    <button>add tp cart</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
