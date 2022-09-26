e6ff5a967d1828f0381d1705d1d0fbc44ad9bfdd
Peggyta <peggyta9096@gmail.com>
2022-09-26 12:52:52 +0330

    if user added an item to shopping basket

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -10,7 +10,17 @@ const initialState = {
 const cartReducer = (state, action) => {
     switch(action.type){
         case "ADD_ITEM":
-            
+            if(!state.selectedItems.find(item => item.id === action.payload.id)) {
+                state.selectedItems.push ({
+                    ...action.payload,
+                    quantity: 1
+                })
+            }
+            return{
+                ...state,
+                selectedItems: [...state.selectedItems],
+                checkout: false
+            }
     }
 }
 
