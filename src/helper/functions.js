31cc060149f49bbf6642b0c9f6c1bfaec37816e3
Peggyta <peggyta9096@gmail.com>
2022-09-26 14:33:43 +0330

    adding some functions for shopping card, in order to add/remove items

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -4,4 +4,18 @@ const shorten = title => {
     return newTitle;
 }
 
-export{shorten};
+const quantityCount = (state, id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id);
+    if (index === -1) {
+        return false
+    }else{
+        return state.selectedItems[index].quantity;
+    }
+}
+
+const isInCart = (state, id) => {
+    const result = !!state.selectedItems.find(item => item.id === id);
+    return result;
+}
+
+export{shorten, quantityCount, isInCart};
