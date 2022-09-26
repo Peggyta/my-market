edae11342b3244dc47229654aa1ae45dff40d034
Peggyta <peggyta9096@gmail.com>
2022-09-26 15:29:15 +0330

    decrease button added

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+
+//Context
+import {CartContext} from '../../context/CartContextProvider';
+
 //Functions
-import {shorten} from '../../helper/functions';
+import {shorten, quantityCount, isInCart} from '../../helper/functions';
+
 
 const Product = ({productData}) => {
+
+    const {state, dispatch} = useContext(CartContext);
+
     return (
         <div>
             <img src={productData.image} alt="product" style={{width:"200px"}}/>
@@ -12,6 +20,8 @@ const Product = ({productData}) => {
             <div>
                 <Link to={`/products/${productData.id}`}>Details</Link>
                 <div>
+                    {quantityCount(state, productData.id) > 1 && <button onClick={() => dispatch({type:"DECREASE", payload: productData})}>-</button>}
+                    
                     <button>add to cart</button>
                 </div>
             </div>
