63666b50c931c745e522019252426e75bea898ad
Peggyta <peggyta9096@gmail.com>
2022-09-26 13:22:19 +0330

    if user decreased items of shopping cart

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -36,6 +36,12 @@ const cartReducer = (state, action) => {
                 ...state
             }
         
+        case "DECREASE":
+            const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id);
+            state.selectedItems[indexD].quantity--;
+            return {
+                ...state
+            }
 
     }
 }
