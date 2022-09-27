5a9903c214688bdac4789a6858bc333534a481f2
Peggyta <peggyta9096@gmail.com>
2022-09-27 14:09:12 +0330

    a spinner added for loading page

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,17 +1,23 @@
 import React, {useContext} from 'react';
 //Components
 import Product from './shared/Product';
+import Loader from './Loader';
 //Context
 import { ProductsContext } from '../context/ProductContextProvider';
 
 const Store = () => {
     const products = useContext(ProductsContext);
     return (
-        <div>
+        <>
+            {products.length ?
+            <div>
             {
                 products.map(product => <Product key={product.id} productData={product}/>)
             }
-        </div>
+            </div> :
+                <Loader />
+            }
+        </>
     );
 };
 
