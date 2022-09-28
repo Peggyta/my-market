6068b6d23c75cb0dfb2377bcc2eada19a225c65f
Peggyta <peggyta9096@gmail.com>
2022-09-28 12:31:05 +0330

    buttons for checkout/clear the shop cart added

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -9,9 +9,19 @@ const ShopCart = () => {
     return (
         <div>
             <div>
-                {state.selectedItems.map(item => <Cart key = {item.id} data = {item} />)}
+                { state.selectedItems.map(item => <Cart key = {item.id} data = {item} />) }
             </div>
-            
+                {
+                    state.itemsCounter > 0 && 
+                    <div> 
+                        <p><span>Total Items:</span>{state.itemsCounter}</p>
+                        <p><span>Total Payments:</span>{state.total}</p>
+                    <div>
+                        <button onClick = {() => dispatch({type:"CHECKOUT"})}>Checkout</button>
+                        <button onClick = {() => dispatch({type:"CLEAR"})}>Clear</button>
+                    </div>
+                    </div>
+                }
         </div>
     );
 };
