ec9cb76f128d8edbf52be389e881661ea4141d6d
Peggyta <peggyta9096@gmail.com>
2022-09-28 12:42:02 +0330

    text messages for buying successfully/want to buy added

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 //Context
 import { CartContext } from '../context/CartContextProvider';
+//Components
 import Cart from './shared/Cart';
 
 const ShopCart = () => {
@@ -16,10 +17,24 @@ const ShopCart = () => {
                     <div> 
                         <p><span>Total Items:</span>{state.itemsCounter}</p>
                         <p><span>Total Payments:</span>{state.total}</p>
-                    <div>
-                        <button onClick = {() => dispatch({type:"CHECKOUT"})}>Checkout</button>
-                        <button onClick = {() => dispatch({type:"CLEAR"})}>Clear</button>
-                    </div>
+                        <div>
+                            <button onClick = {() => dispatch({type:"CHECKOUT"})}>Checkout</button>
+                         <button onClick = {() => dispatch({type:"CLEAR"})}>Clear</button>
+                        </div>
+                        {
+                            state.checkout && 
+                            <div>
+                                <h3>Checkout Successfully</h3>
+                                <Link to ="/products">Buy more</Link>
+                            </div>
+                        }
+                        {
+                            !state.checkout &&
+                            <div>
+                                <h3>Want to buy?</h3>
+                                <Link to ="/products">Go to shop</Link>
+                            </div>
+                        }
                     </div>
                 }
         </div>
