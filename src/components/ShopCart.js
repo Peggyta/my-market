31fa110dadd65bc362bee0f7882e38ac24de9452
Peggyta <peggyta9096@gmail.com>
2022-09-28 12:20:32 +0330

    map on selected items of shopping cart

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+//Context
+import { CartContext } from '../context/CartContextProvider';
+import Cart from './shared/Cart';
 
 const ShopCart = () => {
+    const{ state, dispatch } = useContext(CartContext);
     return (
         <div>
+            <div>
+                {state.selectedItems.map(item => <Cart key = {item.id} data = {item} />)}
+            </div>
             
         </div>
     );
