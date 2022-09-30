587c35445db090ee9b2f1092b963f344c4f24691
Peggyta <peggyta9096@gmail.com>
2022-09-30 11:49:13 +0330

    sytles added for store component

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,18 +4,20 @@ import Product from './shared/Product';
 import Loader from './Loader';
 //Context
 import { ProductsContext } from '../context/ProductContextProvider';
+//Style
+import styles from '../components/Store.module.css';
 
 const Store = () => {
     const products = useContext(ProductsContext);
     return (
         <>
             {products.length ?
-            <div>
-            {
-                products.map(product => <Product key={product.id} productData={product}/>)
-            }
-            </div> :
-                <Loader />
+                <div className={styles.container}>
+                {
+                    products.map(product => <Product key={product.id} productData={product}/>)
+                }
+                </div> :
+                    <Loader />
             }
         </>
     );
